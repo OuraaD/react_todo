8ec035c2eb8ac764fb664da9d52aa02bbb35213d
OuraaD <ouradiarra2@gmail.com>
2024-08-22 15:18:53 +0200

    first commit

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,2 +1,2 @@
 export const API_URL =
-  "https://djxzs77r5e3xeot6q53azoeysm0balnk.lambda-url.eu-north-1.on.aws/task";
+  "https://2pxnc4r76gu63nz2dotifvauaq0aoqfb.lambda-url.eu-north-1.on.aws/task";
